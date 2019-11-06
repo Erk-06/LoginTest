1df4d76eee90e11839b3badcfded2fa806da5b78
Erk-06 <erdi.karabulut@erkteknoloji.com>
2019-11-06 12:22:33 +0300

    LoginActivity.kt HoşgeldinizTexti eklendi

--- app/src/main/java/com/e/loginttest/ui/login/LoginActivity.kt
+++ app/src/main/java/com/e/loginttest/ui/login/LoginActivity.kt
@@ -103,7 +103,7 @@ class LoginActivity : AppCompatActivity() {
         // TODO : initiate successful logged in experience
         Toast.makeText(
             applicationContext,
-            "$welcome $displayName",
+            "Hoşgeldiniz $welcome $displayName",
             Toast.LENGTH_LONG
         ).show()
     }
